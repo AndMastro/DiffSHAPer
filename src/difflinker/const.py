6e1590c1f0620131fc5be9c16c1733eeec84c8d8
AndMastro <andrea.mastropietro@gmail.com>
2025-05-28 13:39:33 +0200

    Setting relative paths

--- src/difflinker/const.py
+++ src/difflinker/const.py
@@ -1,10 +1,13 @@
 import torch
 import yaml
 from rdkit import Chem
+import os
 
 
-# Load configuration from config.yml
-with open('config.yml', 'r') as file:
+# Load configuration from config.yml in the project root directory
+project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+config_path = os.path.join(project_root, 'config.yml')
+with open(config_path, 'r') as file:
     config = yaml.safe_load(file)
     
 RUNNING_DEVICE = config["DEVICE"] if torch.cuda.is_available() else 'cpu'
